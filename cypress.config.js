84264dbf245a141302f22589f51312bb1019ceb9
Leandro Reis <leohbr@gmail.com>
2024-03-21 08:52:52 -0300

    try config @badeball

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,22 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor")
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor")
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild")
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
     baseUrl: "https://automationpratice.com.br/",
     defaultCommandTimeout: 50000,
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor",
+    setupNodeEvents(on, config) {
+      addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
-        }));
+        })
+      );
       return config;
     },
   },
